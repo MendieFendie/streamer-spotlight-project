f6956c55fe0f57637eb42309278a7cb7babab898
Oleh Savaryn <olegsavaryn@gmail.com>
2023-08-13 21:13:14 +0200

    btns

--- src/components/StreamerCard/StreamerCard.js
+++ src/components/StreamerCard/StreamerCard.js
@@ -4,17 +4,21 @@ function StreamerCard(props) {
   const { id, name, platform, description, upvotes, downvotes } = props;
   return (
     <li id={id} className={css.card}>
-      <div>
+      <div className={css.main_group}>
         <div className={css.picture}></div>
         <h3 className={css.name}>{name}</h3>
       </div>
-      <div>
+      <div className={css.description_group}>
         <p className={css.platform}>{platform}</p>
         <p className={css.description}>{description}</p>
       </div>
-      <div>
-        <p className={css.upvotes}>{upvotes}</p>
-        <p className={css.downvotes}>{downvotes}</p>
+      <div className={css.upvotes_group}>
+        <button>
+          <p className={css.upvotes}>{upvotes}</p>
+        </button>
+        <button>
+          <p className={css.downvotes}>{downvotes}</p>
+        </button>
       </div>
     </li>
   );
