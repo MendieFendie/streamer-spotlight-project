9655d1c6bf5266c6bcc6d05670f38d57ea5642fa
Oleh Savaryn <olegsavaryn@gmail.com>
2023-08-15 17:59:26 +0200

    add description h2

--- src/App.js
+++ src/App.js
@@ -12,7 +12,13 @@ function App() {
             <AddBtn />
           </header>
           <main className={css.main}>
-            <h1 className={css.tittle}>Recently Streamers</h1>
+            <h2 className={css.tittle}>
+              Are you a fan of streamers? Do you love watching content on
+              platforms like Twitch, YouTube, TikTok, Kick, or Rumble? Our
+              Streamer Spotlight Application is designed just for you! With this
+              application, you can discover and showcase your favorite streamers
+              while interacting with other users in real-time.
+            </h2>
             <StreamersList />
           </main>
           <footer className={css.footer}></footer>
