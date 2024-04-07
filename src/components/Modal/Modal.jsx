7a69d3eab203689ec0e5559b26eb40090f545a0e
Oleh Savaryn <olegsavaryn@gmail.com>
2024-04-07 21:02:44 +0200

    modal changes

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -9,16 +9,68 @@ function Modal({ openModalHandler }) {
     platform: "Twitch",
     description: "",
     avatar: "",
+    gender: "",
   });
 
   const handleChange = (e) => {
-    e.preventDefault();
     const { name, value } = e.target;
+    let genderValue = "";
+    if (value === "man") {
+      genderValue = `
+short01,
+short02,
+short03,
+short04,
+short05,
+short06,
+short07,
+short08,
+short09,
+short10,
+short11,
+short12,
+short13,
+short14,
+short15,
+short16,
+short17,
+short18,
+short19,
+short20,
+short21,
+short22,
+short23,
+short24`;
+    } else if (value === "woman") {
+      genderValue = `long01,
+long02,
+long03,
+long04,
+long05,
+long06,
+long07,
+long08,
+long09,
+long10,
+long11,
+long12,
+long13,
+long14,
+long15,
+long16,
+long17,
+long18,
+long19,
+long20,
+long21`;
+    }
+
     setForm((prevForm) => ({
       ...prevForm,
       [name]: value,
-      avatar: `https://api.dicebear.com/8.x/pixel-art/svg?seed=${value}`,
+      avatar: `https://api.dicebear.com/8.x/pixel-art/svg?seed=${value}&hair=${genderValue}`,
     }));
+    console.log(form.avatar);
   };
 
   async function handleSubmit(e) {
@@ -29,6 +81,7 @@ function Modal({ openModalHandler }) {
       name: "",
       platform: "",
       description: "",
+      gender: "",
     });
     openModalHandler();
   }
@@ -44,6 +97,27 @@ function Modal({ openModalHandler }) {
           <form onSubmit={handleSubmit}>
             <label htmlFor="name">Streamer name</label>
             <input onChange={handleChange} type="text" name="name" id="name" />
+            <p className={css.gender}>Gender:</p>
+            <label htmlFor="gender">
+              <input
+                onChange={handleChange}
+                type="radio"
+                name="gender"
+                id="man"
+                value="man"
+              />
+              Man
+            </label>
+            <label htmlFor="gender">
+              <input
+                onChange={handleChange}
+                type="radio"
+                name="gender"
+                id="woman"
+                value="woman"
+              />
+              Woman
+            </label>
             <label htmlFor="platform">Platform</label>
             <select onChange={handleChange} id="platform" name="platform">
               <option value="Twitch">Twitch</option>
@@ -52,7 +126,6 @@ function Modal({ openModalHandler }) {
               <option value="Kick">Kick</option>
               <option value="Rumble">Rumble</option>
             </select>
-
             <textarea
               onChange={handleChange}
               name="description"
@@ -61,7 +134,6 @@ function Modal({ openModalHandler }) {
               rows="10"
               placeholder="Description"
             ></textarea>
-
             <button type="submit">Submit</button>
           </form>
         </div>
